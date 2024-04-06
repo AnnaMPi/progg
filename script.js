c17bc4b2fc107dfa20efe0fd31c88e94de8fedb3
AnnaMPi <131648878+AnnaMPi@users.noreply.github.com>
2024-04-06 22:02:28 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+function Randomimage() {
+    let randomsk = Math.floor(Math.random() * 10) + 1;
+
+    let imgran = document.querySelector("img").setAttribute("src","trash-"+randomsk+".png");
+    imgran.style.display = 'block';
+  }
+huij sasi bla jobani ti rot 
+nenavižu cebja 
+neļubļu cibja bolše 
+pašol ti vžobu ti gropani nasos
