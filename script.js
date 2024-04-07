2561fc9f0d54d166dffec42f63f66add74dd1f78
JulijaBeikmane <131656982+JulijaBeikmane@users.noreply.github.com>
2024-04-07 23:13:43 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
-function pogaText() {
+function addext() {
     infoText.innerHTML =  `Sveiki ${titleCreator.value}`; // infoText vietā vajadzētu būt annas vieta
 
+}
+document.getElementById("goToPage2").onclick = function() {
+    window.location.href = 'index (1).html';
+};
+
+var d = new Date();
+var n = d.getFullYear();
+var p = d.getDate();
+var days = ["Svētdiena", "Pirmdiena", "Otrdiena", "Trešdiena", "Ceturtdiena", "Piektdiena", "Sestdiena"];
+var months = ["janvāris", "februāris", "marts", "aprīlis", "maijs", "jūnijs", "jūlijs", "augusts", "septembris", "oktobris", "novembris", "decembris"];
+document.getElementById("diena").innerHTML = days[d.getDay()];
+document.getElementById("gads").innerHTML = n;
+document.getElementById("datums").innerHTML = p;
+document.getElementById("menesis").innerHTML = months[d.getMonth()];
+
+
+function addName() {
+    var userInput = document.getElementById("titleCreator").value;
+    var outputText = document.getElementById("press_text");
+    outputText.textContent +=  userInput + " , Hey!" ;
 }
